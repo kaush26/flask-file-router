4b73fa5cf04f95c4258fef28cf8428764ed18cc7
kaush26 <kaushalprasad2611@gmail.com>
2023-11-03 00:05:52 +0530

    version changed++

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fp:
 
 setuptools.setup(
     name="flask_file_router",
-    version="0.0.2",
+    version="0.0.3",
     author="Kaushal Prasad Balmiki",
     description="File Based Routing for Flask Server",
     long_description=readme,
